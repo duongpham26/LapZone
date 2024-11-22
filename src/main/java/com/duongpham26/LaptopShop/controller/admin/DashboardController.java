00e94657455495ea4c160c9e0e712a12f637616c
duongpham26 <duong.pham2617@gmail.com>
2024-11-22 23:46:38 +0700

    add template dashboard

---
+++ src/main/java/com/duongpham26/LaptopShop/controller/admin/DashboardController.java
@@ -0,0 +1,17 @@
+package com.duongpham26.LaptopShop.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class DashboardController {
+   
+   @GetMapping("/admin")
+   public String getDashboard() {
+       return "admin/dashboard/show";
+   }
+   
+
+}
