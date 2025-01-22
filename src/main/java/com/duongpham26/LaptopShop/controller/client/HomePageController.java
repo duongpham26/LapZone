1179c5c4fd7aae5f520a927d2843061b6d834895
duongpham26 <duong.pham2617@gmail.com>
2025-01-22 09:29:47 +0700

    registerDTO

--- src/main/java/com/duongpham26/LaptopShop/controller/client/HomePageController.java
+++ src/main/java/com/duongpham26/LaptopShop/controller/client/HomePageController.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.duongpham26.LaptopShop.domain.Product;
+import com.duongpham26.LaptopShop.domain.RegisterDTO;
 import com.duongpham26.LaptopShop.service.ProductService;
-
-
 
 @Controller
 public class HomePageController {
@@ -20,11 +21,22 @@ public class HomePageController {
         this.productService = productService;
     }
 
-   @GetMapping("/")
+    @GetMapping("/")
     public String getHomePage(Model model) {
-        List<Product> products = this.productService.getAllProducts(); 
+        List<Product> products = this.productService.getAllProducts();
         model.addAttribute("products", products);
         return "client/homePage/show";
     }
-    
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        model.addAttribute("registerUser", new RegisterDTO());
+        return "client/auth/register";
+    }
+
+    @PostMapping("/register")
+    public String handleRegister(@ModelAttribute("registerUser") RegisterDTO registerDTO) {
+
+        return "client/auth/register";
+    }
 }
