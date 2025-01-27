09944785e49c10e388fe5201391ee8f23b9cf96f
duongpham26 <duong.pham2617@gmail.com>
2025-01-28 00:01:24 +0700

    customuserdetailsservice

--- src/main/java/com/duongpham26/LaptopShop/config/SecurityConfiguration.java
+++ src/main/java/com/duongpham26/LaptopShop/config/SecurityConfiguration.java
@@ -2,17 +2,38 @@ package com.duongpham26.LaptopShop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.duongpham26.LaptopShop.service.CustomUserDetailsService;
+import com.duongpham26.LaptopShop.service.UserService;
+
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
-public class SecurityConfiguration { 
-   
+public class SecurityConfiguration {
+
    @Bean
    public PasswordEncoder passwordEncoder() {
       return new BCryptPasswordEncoder();
    }
-   
+
+   @Bean
+   public UserDetailsService userDetailsService(UserService userService) {
+      return new CustomUserDetailsService(userService);
+   }
+
+   @Bean
+   public DaoAuthenticationProvider authProvider(
+         PasswordEncoder passwordEncoder,
+         UserDetailsService userDetailsService) {
+      DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+      authProvider.setUserDetailsService(userDetailsService);
+      authProvider.setPasswordEncoder(passwordEncoder);
+      // authProvider.setHideUserNotFoundExceptions(false); // Kem an toan, vi hien
+      // thi kh co user
+      return authProvider;
+   }
 }
--- src/main/java/com/duongpham26/LaptopShop/repository/UserRepository.java
+++ src/main/java/com/duongpham26/LaptopShop/repository/UserRepository.java
@@ -19,6 +19,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
    void deleteById(long id);
 
-   public boolean existsByEmail(String email);
+   boolean existsByEmail(String email);
+
+   User findOneByEmail(String email);
 
 }
